9d070a22d894e525c490742bec735bc4b8f650a5
Srk2525 <sanketkanade2525@gmail.com>
2023-10-14 05:05:20 -0700

    Adding two numbers

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your first number:" );
+        int num_1 = sc.nextInt();
+
+        System.out.println("enter our second number :");
+        int num_2 = sc.nextInt();
+
+        int sum = num_1 + num_2 ;
+        System.out.println("Sum: " + sum);
+
+
+    }
+}
